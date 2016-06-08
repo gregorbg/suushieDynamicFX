c11226ebfe4b80f35460d028123046ccb9fe4a5d
suushie_maniac <suushiemaniac@gmail.com>
2016-06-08 15:53:36 +0200

    Implement and specify NodeParser

--- src/com/suushiemaniac/gui/javafx/dynamic/NodeParser.java
+++ src/com/suushiemaniac/gui/javafx/dynamic/NodeParser.java
@@ -1,32 +1,19 @@
 package com.suushiemaniac.gui.javafx.dynamic;
 
 import com.suushiemaniac.lang.json.JSON;
+import com.suushiemaniac.lang.json.util.StringUtils;
 import com.suushiemaniac.lang.json.value.JSONType;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.layout.Pane;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 public class NodeParser {
     public static final String HEADER = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
-
-    private static Set<String> gatherImports(JSONType jsonNode) {
-        Set<String> allImports = new HashSet<>();
-        allImports.add(jsonNode.get("Type").stringValue());
-
-        JSONType children = jsonNode.get("Children");
-
-        if (children != null) {
-            for (JSONType child : children) {
-                allImports.addAll(gatherImports(child));
-            }
-        }
-
-        return allImports;
-    }
 
     private FXMLLoader loader;
 
@@ -34,9 +21,30 @@ public class NodeParser {
         this.loader = new FXMLLoader();
     }
 
-    public <T extends Node> T parseFromJSON(JSON jsonNode) throws IOException {
-        Set<String> importSet = gatherImports(jsonNode);
-        return this.parse(String.join("\n", HEADER, String.join("\n", importSet), jsonNode.toXMLString()));
+    /**
+     * {
+     *     "xmlImports" : [listWithAllPreFormattedXMLImports],
+     *     "element" : "TheELementXMLAsJSONRep",
+     *     "children" : [optionalChildren]
+     * }
+     *
+     * @param jsonNode The node containing all details about the FXML element to be loaded
+     * @return The JavaFX GUI scene graph element in the closest requested type possible
+     * @throws IOException When the FXML parser is on strike
+     */
+    public <T extends Node> T parseFromJSON(JSONType jsonNode) throws IOException {
+        Collection<String> xmlImports = jsonNode.get("xmlImports").collect().stream().map(s -> StringUtils.jsonUnwrap(s.toString())).collect(Collectors.toList());
+        T element = this.parse(String.join("\n", HEADER, String.join("\n", xmlImports), jsonNode.get("element").toXMLString()));
+
+        if (element instanceof Pane) {
+            JSONType children = jsonNode.getOrDefault("children", JSON.fromJSONString("[]"));
+            
+            for (JSONType child : children) {
+                ((Pane) element).getChildren().add(parseFromJSON(child));
+            }
+        }
+
+        return element;
     }
 
     public <T extends Node> T parse(String fxmlString) throws IOException {
