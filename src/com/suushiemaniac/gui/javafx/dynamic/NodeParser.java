2bc4b08af8bb3ed492245b2d86dfa7d9a127e28a
suushie_maniac <suushiemaniac@gmail.com>
2016-06-09 00:26:56 +0200

    Make NodeParser imports dynamic

--- src/com/suushiemaniac/gui/javafx/dynamic/NodeParser.java
+++ src/com/suushiemaniac/gui/javafx/dynamic/NodeParser.java
@@ -1,7 +1,6 @@
 package com.suushiemaniac.gui.javafx.dynamic;
 
 import com.suushiemaniac.lang.json.JSON;
-import com.suushiemaniac.lang.json.util.StringUtils;
 import com.suushiemaniac.lang.json.value.JSONType;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -9,11 +8,20 @@ import javafx.scene.layout.Pane;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.util.Collection;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.List;
 
 public class NodeParser {
     public static final String HEADER = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
+
+    public static boolean isClass(String className) {
+        try {
+            Class.forName(className);
+            return true;
+        } catch (ClassNotFoundException e) {
+            return false;
+        }
+    }
 
     private FXMLLoader loader;
 
@@ -23,8 +31,7 @@ public class NodeParser {
 
     /**
      * {
-     *     "xmlImports" : [listWithAllPreFormattedXMLImports],
-     *     "element" : "TheELementXMLAsJSONRep",
+     *     "element" : "TheElementXMLAsJSONRep",
      *     "children" : [optionalChildren]
      * }
      *
@@ -33,8 +40,25 @@ public class NodeParser {
      * @throws IOException When the FXML parser is on strike
      */
     public <T extends Node> T parseFromJSON(JSONType jsonNode) throws IOException {
-        Collection<String> xmlImports = jsonNode.get("xmlImports").collect().stream().map(s -> StringUtils.jsonUnwrap(s.toString())).collect(Collectors.toList());
-        T element = this.parse(String.join("\n", HEADER, String.join("\n", xmlImports), jsonNode.get("element").toXMLString()));
+        List<String> xmlImports = new ArrayList<>();
+
+        JSONType jsonElement = jsonNode.get("element");
+        for (JSONType elementType : jsonElement) {
+            for (Package p : Package.getPackages()) {
+                String pName = p.getName();
+
+                if (pName.startsWith("javafx")) {
+                    String elementName = jsonElement.keyIndexOf(elementType).stringValue();
+                    String cName = pName + "." + elementName;
+
+                    if (isClass(cName)) {
+                        xmlImports.add("<?import " + cName + "?>");
+                    }
+                }
+            }
+        }
+
+        T element = this.parse(String.join("\n", HEADER, String.join("\n", xmlImports), jsonElement.toXMLString()));
 
         if (element instanceof Pane) {
             JSONType children = jsonNode.getOrDefault("children", JSON.fromJSONString("[]"));
@@ -48,6 +72,7 @@ public class NodeParser {
     }
 
     public <T extends Node> T parse(String fxmlString) throws IOException {
+        for (Package p : Package.getPackages()) if (p.getName().startsWith("javafx")) System.out.println(p.getName());
         return this.loader.load(new ByteArrayInputStream(fxmlString.getBytes()));
     }
 }
